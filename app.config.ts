3d213428e98d0d3cba875d49ff7d76fe8dafa783
Harry Vince <harryavince@gmail.com>
2025-06-08 21:11:34 +0100

    feat: add top-level-await support if required and inject react-scan in dev environments

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import reactRefresh from "@vitejs/plugin-react";
 import { createApp } from "vinxi";
+import topLevelAwait from "vite-plugin-top-level-await";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default createApp({
@@ -43,8 +44,24 @@ export default createApp({
           routesDirectory: "./src/routes",
           generatedRouteTree: "./src/routeTree.gen.ts",
         }),
-        reactRefresh(),
         tsConfigPaths({ projects: ["./tsconfig.json"] }),
+        topLevelAwait({
+          promiseExportName: "__tla",
+          promiseImportName: (i) => `__tla_${i}`,
+        }),
+        {
+          name: "inject-react-scan-dev",
+          transformIndexHtml(html: string) {
+            if (process.env.NODE_ENV !== "production") {
+              return html.replace(
+                "</body>",
+                '<script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js"></script></body>',
+              );
+            }
+            return html;
+          },
+        },
+        reactRefresh(),
         tailwindcss(),
       ],
     },
